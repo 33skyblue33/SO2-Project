e34925979c8a52c2211a26b304b692d42273fe58
Alicja Fuks <fuksalicja21@gmail.com>
2025-03-26 15:45:43 +0100

    fixed some formatting

--- Mutex.cpp
+++ Mutex.cpp
@@ -1,16 +1,15 @@
+//Copyright[2025] Alicja Fuks
 #include "Mutex.h"
 
 //Locks the mutex and returns control to the main thread
-void Mutex::lock()
-{
-	while (locked.test_and_set(memory_order_acquire))
-	{
-		this_thread::yield();
-	}
-}
+
+void Mutex::lock() {
+    while (locked.test_and_set(memory_order_acquire)) {
+        this_thread::yield();
+    }
+}  
 
 //Unlocks the mutex and frees the related resources for other threads
-void Mutex::unlock()
-{
-	locked.clear(memory_order_release);
+void Mutex::unlock() {
+    locked.clear(memory_order_release);
 }
